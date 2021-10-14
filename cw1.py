a2cdb8a10f38dea2667b849b647148feb5693cbb
tomilidzons20 <73526217+tomilidzons20@users.noreply.github.com>
2021-10-14 15:14:55 +0200

    Add files via upload

---
+++ cw1.py
@@ -0,0 +1,87 @@
+# def zad1(litera, nazwisko):
+#     print(litera + "." + nazwisko)
+#
+#
+# zad1("j", "pietkiewicz")
+#
+#
+# def zad2(imie, nazwisko):
+#     print(imie[0].capitalize() + "." + nazwisko.capitalize())
+#
+#
+# zad2("jakub", "pietkiewicz")
+#
+#
+# def zad3(pierwsze, ostatnie, wiek):
+#     print("data ur:", pierwsze * 100 + ostatnie - wiek)
+#
+#
+# zad3(20, 21, 21)
+#
+#
+# def zad4(imie, nazwisko, funkcja):
+#     funkcja(imie, nazwisko)
+#
+#
+# zad4("Jakub", "Pietkiewicz", zad2)
+#
+#
+# def zad5(l1, l2):
+#     if l2 != 0 and l1 > 0 and l2 > 0 and l2 != 0:
+#         print(l1 / l2)
+#
+#
+# zad5(4, 3)
+#
+#
+# def zad6():
+#     print("Podawaj liczby dopoki ich suma nie bedzie wynosic min 100")
+#     suma = 0
+#     while suma < 100:
+#         liczba = input()
+#         suma += int(liczba)
+#     print(suma)
+#
+# zad6()
+#
+#
+# def zad7(lista):
+#     krotka = tuple(lista)
+#     print(krotka)
+#
+#
+# lista1 = [1, 5, 8, "klocek"]
+# zad7(lista1)
+#
+#
+# def zad8():
+#     lista = []
+#     while(len(lista) < 6):
+#         lista.append(int(input()))
+#     krotka = tuple(lista)
+#     print(krotka)
+#
+#
+# zad8()
+#
+#
+# def zad9(liczba):
+#     lista = ["poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela"]
+#     print(lista[liczba - 1])
+#
+#
+# zad9(6)
+#
+#
+# def zad10(znaki):
+#     odw = znaki[::-1]
+#     if(znaki == odw):
+#         print("jest palindromem")
+#     else:
+#         print("nie jest palindromem")
+#
+#
+# zad10("kajak")
+
+
+
