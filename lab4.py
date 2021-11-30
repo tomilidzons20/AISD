cb414bf6db8614bd899782f03b15233b1ccec684
tomilidzons20 <73526217+tomilidzons20@users.noreply.github.com>
2021-11-30 17:38:57 +0100

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,7 +1,11 @@
-from typing import Any, List, Callable, Union
+from collections import deque
+from typing import Any, Callable, List, Union
+
+import treelib as tr
+
 
 class TreeNode:
-    def __init__(self, value: Any =None):
+    def __init__(self, value: Any):
         self.value: Any = value
         self.children: List['TreeNode'] = []
     
@@ -13,44 +17,112 @@ class TreeNode:
     def add(self, child: 'TreeNode') -> None:
         self.children.append(child)
 
-    def visit(self, peak: 'TreeNode', child: 'TreeNode') -> None:
-        return self.visit(peak, self.children[child+1])
-
     def for_each_deep_first(self, visit: Callable[['TreeNode'], None]) -> None:
-        return self.for_each_deep_first(visit(self, self.children))
+        if not self:
+            return
+        visit(self)
+        if not self.is_leaf():
+            for child in self.children:
+                child.for_each_deep_first(visit)
 
-    def for_each_level_order(visit: Callable[['TreeNode'], None]) -> None:
-        pass
+    def for_each_level_order(self, visit: Callable[['TreeNode'], None]) -> None:
+        if not self:
+            return
+        visit(self)
+        fifo = deque()
+        for child in self.children:
+            fifo.append(child)
+        while fifo:
+            i = fifo.popleft()
+            visit(i)
+            for child in i.children:
+                fifo.append(child)
 
-    def search(value: Any) -> Union['TreeNode', None]:
-        pass
+    def search(self, value: Any) -> Union['TreeNode', None]:
+        def search_2(node: 'TreeNode') -> None:
+            if node.value == value:
+                res.append(node)
+        res: List['TreeNode'] = []
+        self.for_each_level_order(search_2)
+        if res:
+            return res[0]
+        return None
 
-    def print(self) -> None:
-        print(self.value)
+    def __str__(self) -> str:
+        return str(self.value)
+    
+    # def print_tree(self) -> None:
+        # self.for_each_deep_first(self.__str__())
+        # self.for_each_level_order(self.print(self))
 
+        # deep first
+        # self.print()
+        # if not self.is_leaf():
+        #     for child in self.children:
+        #         child.print_tree()
 
-tree = TreeNode(4)
-print(tree.is_leaf())
-tree.add(TreeNode(3))
-print(tree.is_leaf())
-tree.print()
+        # level order
+        # if not self:
+        #     return
+        # fifo = deque()
+        # fifo.append(self)
+        # while fifo:
+        #     i = fifo.popleft()
+        #     i.print()
+        #     if i.children:
+        #         for child in i.children:
+        #             fifo.append(child)
 
 class Tree:
     def __init__(self, root: 'TreeNode'):
-        root: TreeNode = root
+        self.root: TreeNode = root
     
-    def add(value: Any, parent_name: Any) -> None:
-        pass
+    def add(self, value: Any, parent_name: 'TreeNode') -> None:
+        def search(node: 'TreeNode') -> None:
+            if node == parent_name:
+                res[0] = True
+        res: List[bool] = [False]
+        self.for_each_level_order(search)
+        if res[0] == True:
+            parent_name.add(TreeNode(value))  
 
-    def for_each_level_order(visit: Callable[['TreeNode'], None]) -> None:
-        pass
+    def for_each_level_order(self, visit: Callable[['TreeNode'], None]) -> None:
+        self.root.for_each_level_order(visit)
 
-    def for_each_deep_first(visit: Callable[['TreeNode'], None]) -> None:
-        pass
+    def for_each_deep_first(self, visit: Callable[['TreeNode'], None]) -> None:
+        self.root.for_each_deep_first(visit)
 
-    def show():
-        pass
+    def show(self) -> None:
+        tl = tr.Tree()
+        tl.create_node(str(self.root), str(self.root))
+        def edge(node: 'TreeNode') -> None:
+            for child in node.children:
+                tl.create_node(str(child), str(child), parent=str(node))
+        self.for_each_deep_first(edge)
+        tl.show()
 
+tree = Tree(TreeNode(4))
+c1 = TreeNode(3)
+c11 = TreeNode(6)
+c111 = TreeNode(0)
+c12 = TreeNode(2)
+c122 = TreeNode(10)
+c11.add(c111)
+c12.add(c122)
+c1.add(c11)
+c1.add(c12)
 
-dupa = [1,2,3,4]
-print(dupa[2])
+c2 = TreeNode(5)
+c21 = TreeNode(7)
+c211 = TreeNode(9)
+c2.add(c21)
+c21.add(c211)
+c3 = TreeNode("osiem")
+
+tree.root.add(c1)
+tree.root.add(c2)
+tree.root.add(c3)
+tree.add(1, c2)
+tree.add("siedem", c3)
+
+tree.show()
