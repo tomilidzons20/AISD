9450132ce6acf66b68de792313e1858640bae93f
tomilidzons20 <73526217+tomilidzons20@users.noreply.github.com>
2021-12-14 17:22:28 +0100

    Update projekt2.py

--- projekt2.py
+++ projekt2.py
@@ -1,5 +1,6 @@
 from typing import Any, Callable, List
-import treelib as tr
+from testing import Queue
+import binarytree as bt
 
 
 class BinaryNode:
@@ -43,6 +44,7 @@ class BinaryNode:
 
     def __str__(self) -> str:
         return str(self.value)
+    
 
 
 class BinaryTree:
@@ -59,16 +61,52 @@ class BinaryTree:
         self.root.traverse_pre_order(visit)
 
     def show(self) -> None:
-        def edge(node: 'BinaryNode') -> None:
-            if node.left_child is not None:
-                tl.create_node(str(node.left_child), str(node.left_child), str(node))
-            if node.right_child is not None:
-                tl.create_node(str(node.right_child), str(node.right_child), str(node))
-        tl = tr.Tree()
-        tl.create_node(str(self.root), str(self.root))
-        self.traverse_pre_order(edge)
-        tl.show()   
-        
+        def level_order(node: BinaryNode, visit: Callable[[Any], None]) -> None:
+            if node is None:
+                return
+            visit(node)
+            fifo = Queue()
+            nc = 0 # number of nones in level
+            level = 0
+            fifo.enqueue(node.left_child)
+            fifo.enqueue(node.right_child)
+            if node.left_child is None:
+                nc += 1
+            if node.right_child is None:
+                nc += 1
+            while fifo:
+                level += 1
+                if nc == 2**(level): 
+                    # if number of nones in level is equal to numbers of 
+                    # nodes on this level in full binary tree then tree ends
+                    break
+                nc = 0 # reset number of nones on level
+                levelcount = len(fifo) # count number of nodes on level
+                while levelcount > 0:
+                    i = fifo.dequeue()
+                    visit(i)
+                    levelcount -= 1
+                    if i is not None:
+                        fifo.enqueue(i.left_child)
+                        fifo.enqueue(i.right_child)
+                        if i.left_child is None:
+                            nc += 1
+                        if i.right_child is None:
+                            nc += 1
+                    if i is None:
+                        fifo.enqueue(None)
+                        fifo.enqueue(None)
+                        nc += 2
+        def help(node: BinaryNode) -> None:
+            if node is None:
+                nodes.append(None)
+                return
+            nodes.append(node.value)
+        nodes = []
+        level_order(self.root, help)
+        # print(nodes)
+        print(bt.build(nodes))   
+
 
 # tree = BinaryTree(10)
 # tree.root.add_left_child(9)
@@ -84,44 +122,104 @@ class BinaryTree:
 # assert tree.root.left_child.left_child.value == 1
 # assert tree.root.left_child.left_child.is_leaf() is True
 
-tree = BinaryTree(1)
-tree.root.add_left_child(2)
-tree.root.add_right_child(3)
-tree.root.left_child.add_left_child(4)
-tree.root.left_child.add_right_child(5)
-tree.root.left_child.left_child.add_left_child(8)
-tree.root.left_child.left_child.add_right_child(9)
-tree.root.right_child.add_right_child(7)
+# tree = BinaryTree(1)
+# tree.root.add_left_child(2)
+# tree.root.add_right_child(3)
+# tree.root.left_child.add_left_child(4)
+# tree.root.left_child.add_right_child(5)
+# tree.root.left_child.left_child.add_left_child(8)
+# tree.root.left_child.left_child.add_right_child(9)
+# tree.root.right_child.add_right_child(7)
+# tree.root.right_child.right_child.add_left_child(10)
 
-t1 = tree.root.left_child #2
-t2 = tree.root.right_child #3
-t3 = tree.root.left_child.left_child #4
-t4 = tree.root.left_child.right_child #5
-t5 = tree.root.left_child.left_child.left_child #8
-t6 = tree.root.left_child.left_child.right_child #9
-t7 = tree.root.right_child.right_child #7
+# t1 = tree.root.left_child #2
+# t2 = tree.root.right_child #3
+# t3 = tree.root.left_child.left_child #4
+# t4 = tree.root.left_child.right_child #5
+# t5 = tree.root.left_child.left_child.left_child #8
+# t6 = tree.root.left_child.left_child.right_child #9
+# t7 = tree.root.right_child.right_child #7
+# t8 = tree.root.right_child.right_child.left_child
 
-tree.show()
+# tree.show()
 
 
 def check_level(tree: BinaryTree, first_node: BinaryNode, second_node: BinaryNode) -> bool:
-    def help(node: BinaryNode, snode: BinaryNode, level: int) -> int:
-        if node == snode:
-            return level
-        levelhelp = 0
+    def level_order(node: BinaryNode) -> None:
+        if node is None:
+            return
+        level = 0
+        nodes(node) # append nodelist with tree.root
+        levels(level) # append levellist with 0
+        fifo = Queue()
         if node.left_child is not None:
-            levelhelp = help(node.left_child, snode, level + 1)
-        if levelhelp != 0:
-            return levelhelp
+            fifo.enqueue(node.left_child)
         if node.right_child is not None:
-            levelhelp = help(node.right_child, snode, level + 1)
-        return levelhelp
-    levelf = help(tree.root, first_node, 0)
-    levels = help(tree.root, second_node, 0)
-    if levelf == levels:
-        return True
-    else:
-        return False
+            fifo.enqueue(node.right_child)
+        while fifo:
+            level += 1
+            levelcount = len(fifo) # number of nodes on the same level
+            while levelcount > 0:
+                i = fifo.dequeue()
+                nodes(i) # append nodelist with node
+                levels(level) # append levellist with current level
+                # node and its level are on the same position in both lists
+                levelcount -= 1 # decrease number of nodes on the same leevel
+                if i.left_child is not None:
+                    fifo.enqueue(i.left_child)
+                if i.right_child is not None:
+                    fifo.enqueue(i.right_child)
+    def nodes(node: BinaryNode):
+        nodelist.append(node)
+    def levels(level: int):
+        levellist.append(level)
+    nodelist: List[BinaryNode] = []
+    levellist: List[int] = []
+    level_order(tree.root)
+    if nodelist:
+        for i in range(len(nodelist)):
+            if nodelist[i] == first_node:
+                fnl = levellist[i] # level of first_node
+            if nodelist[i] == second_node:
+                snl = levellist[i] # level of second_node
+        if fnl == snl:
+            return True
+    return False
 
 
-print(check_level(tree, t4, t7))
+# print(check_level(tree, t3, t8))
+
+tree = BinaryTree(100)
+tree.root.add_left_child(10)
+tree.root.left_child.add_left_child(20)
+tree.root.left_child.add_right_child(8)
+tree.root.left_child.left_child.add_left_child(25)
+tree.root.left_child.left_child.add_right_child(15)
+tree.root.left_child.left_child.left_child.add_left_child(16)
+tree.root.left_child.left_child.left_child.add_right_child(31)
+tree.root.left_child.left_child.right_child.add_left_child(17)
+tree.root.left_child.left_child.right_child.add_right_child(21)
+tree.root.add_right_child(45)
+tree.root.right_child.add_left_child(53)
+tree.root.right_child.add_right_child(70)
+tree.root.right_child.right_child.add_left_child(81)
+tree.root.right_child.right_child.add_right_child(24)
+
+n1 = tree.root.left_child # 10
+n2 = tree.root.right_child # 45
+n3 = tree.root.left_child.left_child # 20
+n4 = tree.root.left_child.right_child # 8
+n5 = tree.root.right_child.left_child # 53
+n6 = tree.root.right_child.right_child # 70
+n7 = tree.root.left_child.left_child.left_child # 25
+n8 = tree.root.left_child.left_child.right_child # 15
+n9 = tree.root.right_child.right_child.left_child # 81
+n10 = tree.root.right_child.right_child.right_child # 24
+n11 = tree.root.left_child.left_child.left_child.left_child # 16
+n12 = tree.root.left_child.left_child.left_child.right_child # 31
+n13 = tree.root.left_child.left_child.right_child.left_child # 17
+n14 = tree.root.left_child.left_child.right_child.right_child # 21
+
+tree.show()
+
+print(check_level(tree, n10, n8))
